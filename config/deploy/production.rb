a3620f7abb2f0057026fb838756540321d1e3ca9
obSYO <sho020202forest@gmail.com>
2019-12-07 21:14:26 +0900

    miss_spell  Correction

--- config/deploy/production.rb
+++ config/deploy/production.rb
@@ -59,4 +59,4 @@
 #     auth_methods: %w(publickey password)
 #     # password: "please use keys"
 #   }
-server '<18.182.180.91>', user: 'ec2-user', roles: %w{app db web}
+server '18.182.180.91', user: 'ec2-user', roles: %w{app db web}
